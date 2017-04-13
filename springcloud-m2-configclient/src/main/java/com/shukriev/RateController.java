f86453407d0bdce5f0df9af89b559131401c4fc3
Shukri Shukriev <dark00v3@gmail.com>
2017-04-13 16:25:30 +0300

    spring cloud client configuration

---
+++ springcloud-m2-configclient/src/main/java/com/shukriev/RateController.java
@@ -0,0 +1,26 @@
+package com.shukriev;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RateController {
+	@Value("${rate}")
+	String rate;
+	
+	@Value("${lanecount}")
+	String lanecount;
+	
+	@Value("${tollstart}")
+	String tollstart;
+	
+	@RequestMapping("/rate")
+	public String getRate(Model m){
+		m.addAttribute("rateamount", rate);
+		m.addAttribute("lanecount", lanecount);
+		m.addAttribute("tollstart", tollstart);
+		return "rateview";
+	}
+}
